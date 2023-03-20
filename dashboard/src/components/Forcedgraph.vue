c23dddc3756123f93c983d1fc033936c241e3121
Hongxiang Zhang <hongxiangzhang@Mac-Boulevard.local>
2023-03-19 21:48:59 -0700

    fix zoom func

--- dashboard/src/components/Forcedgraph.vue
+++ dashboard/src/components/Forcedgraph.vue
@@ -25,7 +25,7 @@ export default {
             size: { width: 700, height: 700 } as ComponentSize, // size of graph
             margin: { left: 10, right: 10, top: 10, bottom: 10 } as Margin,
             forcedstrength: -10 as number,
-            forcedgraphradius: 15 as number,
+            forcedgraphradius: 6 as number,
             graph_id_tmp: -1,
             instanceEmb: [],
             instanceGraphs: [],
@@ -55,7 +55,7 @@ export default {
                     return true;
                 })
                 .catch(error => console.log(error));
-            
+
             axios.get(`${server}/fetchInstance`)
                 .then(resp => {
                     this.instanceEmb = resp.data.data;
@@ -131,7 +131,7 @@ export default {
                 .force('link', d3.forceLink(links).id(d => d.id))
                 .force('charge', d3.forceManyBody().strength(this.forcedstrength)) // Forced strength
                 .force('collide', d3.forceCollide().radius(this.forcedgraphradius))
-                .force('center', d3.forceCenter((width-this.margin.left-this.margin.right)/2, (height-this.margin.top-this.margin.bottom)/2)); // Forced center
+                .force('center', d3.forceCenter((width - this.margin.left - this.margin.right) / 2, (height - this.margin.top - this.margin.bottom) / 2)); // Forced center
 
             const g = chartContainer.append('g').attr('class', 'everything');
 
@@ -155,7 +155,7 @@ export default {
                 .on("click", click_node)
                 .on("mouseover", hover_node)
                 .on("mouseout", out_node)
-                .call(drag(simulation)); // Drag function
+                .on(drag(simulation)); // Drag function
 
             // node.append('text')
             //     .text(d => d.id)
@@ -163,7 +163,7 @@ export default {
             //     .attr('opacity', 0.8)
             //     .attr('x', 2)
             //     .attr('y', 2);
-            
+
             function click_node() {
                 if (chartThis.node_selected_id == -1) {
                     d3.select(this).transition()
@@ -210,54 +210,6 @@ export default {
                 }
             };
 
-            // Lasso functions
-            var lasso_start = function () {
-                lasso.items()
-                    .attr("r", 3.5) // reset size
-                    .classed("not_possible", true)
-                    .classed("selected", false);
-            };
-
-            var lasso_draw = function () {
-
-                // Style the possible dots
-                lasso.possibleItems()
-                    .classed("not_possible", false)
-                    .classed("possible", true);
-
-                // Style the not possible dot
-                lasso.notPossibleItems()
-                    .classed("not_possible", true)
-                    .classed("possible", false);
-            };
-
-            var lasso_end = function () {
-                // Reset the color of all dots
-                lasso.items()
-                    .classed("not_possible", false)
-                    .classed("possible", false);
-
-                // Style the selected dots
-                lasso.selectedItems()
-                    .classed("selected", true)
-                    .attr("r", 7);
-
-                // Reset the style of the not selected dots
-                lasso.notSelectedItems()
-                    .attr("r", 3.5);
-
-            };
-            // // BUGS
-            // var lasso = d3Lasso.lasso()
-            //     .closePathSelect(true)
-            //     .closePathDistance(100)
-            //     .items(g.selectAll("circle"))
-            //     .targetArea(g.selectAll("circle"))
-            //     .on("start", lasso_start)
-            //     .on("draw", lasso_draw)
-            //     .on("end", lasso_end);
-            // lasso(svg);
-
             simulation.on('tick', () => {
                 link
                     .attr('x1', d => d.source.x)
@@ -267,12 +219,12 @@ export default {
                 node.attr('transform', d => `translate(${d.x},${d.y})`);
             });
 
-            // Zoom functions
-            const zoomHandler = d3.zoom().on('zoom', zoomActions);
-            zoomHandler(chartContainer)
-            function zoomActions() {
-                g.attr('transform', d3.event.transform)
-            }
+            // zoom functions
+            const handleZoom = (e) => g.attr('transform', e.transform);
+
+            const zoom = d3.zoom().on('zoom', handleZoom);
+
+            d3.select('#canvas-svg').call(zoom);
 
             // drag functions
             function drag(simulation) {
@@ -325,8 +277,8 @@ export default {
             const xDomain_tmp = d3.extent(X);
             const yDomain_tmp = d3.extent(Y);
 
-            const xDomain = [xDomain_tmp[0]-0.05, xDomain_tmp[1]+0.05];
-            const yDomain = [yDomain_tmp[0]-0.05, yDomain_tmp[1]+0.05];
+            const xDomain = [xDomain_tmp[0] - 0.05, xDomain_tmp[1] + 0.05];
+            const yDomain = [yDomain_tmp[0] - 0.05, yDomain_tmp[1] + 0.05];
 
             const xRange = [this.margin.left, chart_size.width - this.margin.right];
             const yRange = [chart_size.height - this.margin.bottom, this.margin.top];
@@ -343,7 +295,7 @@ export default {
                 .call(xAxis)
                 .call(g => g.append('text')
                     .attr('x', this.margin.left + (chart_size.width - this.margin.left - this.margin.right) / 2)
-                    .attr('y', this.margin.bottom-3)
+                    .attr('y', this.margin.bottom - 3)
                     .attr('fill', 'currentColor')
                     .attr('text-anchor', 'center')
                     .text("PCA component=2 node embedding layout"));
@@ -360,8 +312,8 @@ export default {
                 .attr('opacity', 0.3)
                 .attr('cx', i => xScale(X[i]))
                 .attr('cy', i => yScale(Y[i]))
-                .attr('r', i => (this.node_selected_id==-1) ? ((this.node_hover_id==-1) ? 5 : ((this.node_hover_id==i) ? 10 : 5)) : ((this.node_selected_id==i) ? 10 : 5));
-            
+                .attr('r', i => (this.node_selected_id == -1) ? ((this.node_hover_id == -1) ? 5 : ((this.node_hover_id == i) ? 10 : 5)) : ((this.node_selected_id == i) ? 10 : 5));
+
             // chartContainer.append('g')
             //     .selectAll('text')
             //     .data(I)
@@ -394,7 +346,7 @@ export default {
         //             return true;
         //         })
         //         .catch(error => console.log(error));
-            
+
         //     this.initChart();
         // },
     },
@@ -414,15 +366,15 @@ export default {
             <svg id="canvas-svg" width="100%" height="100%"></svg>
         </div>
         <div class="emb-container" ref="embeddingContainer">
-            <svg id="embedding-svg" width="100%" height="100%" ></svg>
+            <svg id="embedding-svg" width="100%" height="100%"></svg>
         </div>
         <!-- <div id=" chart-control-container">
-            <label :style="{ fontSize: '0.7rem' }"> Type graph number[0-1999]:</label><br>
-            <input placeholder="HERE" v-model="graph_num" class="inputbox" /><br>
-            <button @click="buttonClick" class="button">Jump to</button><br>
-            <button @click="buttonClick" class="button">Center</button>
-            <button class="button">Lasso select</button>
-        </div> -->
+                                                    <label :style="{ fontSize: '0.7rem' }"> Type graph number[0-1999]:</label><br>
+                                                    <input placeholder="HERE" v-model="graph_num" class="inputbox" /><br>
+                                                    <button @click="buttonClick" class="button">Jump to</button><br>
+                                                    <button @click="buttonClick" class="button">Center</button>
+                                                    <button class="button">Lasso select</button>
+                                                </div> -->
     </div>
 </template>
 
@@ -440,6 +392,7 @@ export default {
     /* border-width: 2px;
     border-color: #c60d0d; */
 }
+
 .emb-container {
     height: 30%;
     width: 100%;
